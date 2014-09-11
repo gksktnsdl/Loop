2fccb1c113d961da9aa84acbb4a39e6c33d778ec
gksktnsdl <gksktnsdl@naver.com>
2014-09-11 15:21:05 +0900

    s

--- sketch.js
+++ sketch.js
@@ -1,28 +1,19 @@
 function setup() {
   createCanvas(640, 480);
-
-  
-  background(174,138,184);
-  stroke(255);
+  background(128, 0, 64);
   noFill();
- line(360,10,380,10);
-  line(360,10,360,20);
-  line(380,10,380,20);
-  line(350,20,360,20);
-  line(380,20,390,20);
 
-  line(350,20,350,40);
-  line(390,20,390,40);
-  line(380,40,390,40);
-  line(350,40,360,40);
-  line(360,40,360,50);
+  stroke(255, 0, 0);
 
-  line(380,40,380,50);
-  line(360,50,380,50);
-  line(350,50,360,50);
-  line(360,50,360,70);
-  line(350,70,360,70);
-  line(350,50,350,70);
-   rect(20, 20, 80, 50); // x, y, width, height
-    ellipse(50, 50, 50, 50); // centerX, centerY, width, height
+  var x;
+
+  x = 100;
+  ellipse(x, 100, 50, 50);
+
+  x = 250;
+  ellipse(x, 100, 50, 50);
+
+  x = 300;
+  ellipse(x, 100, 50, 50);
+}
 }
